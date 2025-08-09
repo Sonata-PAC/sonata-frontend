6b39f120598c5f52b1cf191c1f243cf3a0860290
Eduardo Martins <eduardobizzi2005@gmail.com>
2025-08-09 01:15:15 -0300

    Linter fixes

--- src/routes/auth/sign-in/+page.svelte
+++ src/routes/auth/sign-in/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-    import LoginCard from 'molecules/loginCard/loginCard.svelte'
-    import { Header } from "organisms/header"
-    import { Footer } from "organisms/footer"
+  import LoginCard from "molecules/loginCard/loginCard.svelte"
+  import { Header } from "organisms/header"
+  import { Footer } from "organisms/footer"
 </script>
 
 <svelte:head>
-	<title>Sign-in</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Sign-in</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section class="w-full flex flex-1 flex-col justify-between items-center">
-    <Header />
-    <LoginCard redirectURL="/host/create" title="Entre com sua conta Microsoft" description="Para organizar uma sessão, faça login com sua conta microsoft" />
-    <Footer />
+<section class="flex w-full flex-1 flex-col items-center justify-between">
+  <Header />
+  <LoginCard
+    redirectURL="/host/create"
+    title="Entre com sua conta Microsoft"
+    description="Para organizar uma sessão, faça login com sua conta microsoft"
+  />
+  <Footer />
 </section>

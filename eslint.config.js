8a63bcdd6e4931ac7728b1fe5fe4cab93a7294fd
Eduardo Martins <eduardobizzi2005@gmail.com>
2025-08-09 00:44:32 -0300

    ESlint and Prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,12 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook"
-
-import prettier from "eslint-config-prettier"
-import { includeIgnoreFile } from "@eslint/compat"
-import js from "@eslint/js"
 import svelte from "eslint-plugin-svelte"
-import globals from "globals"
-import { fileURLToPath } from "node:url"
-import ts from "typescript-eslint"
 import svelteConfig from "./svelte.config.js"
+import js from "@eslint/js"
+import ts from "typescript-eslint"
+import globals from "globals"
+import prettier from "eslint-config-prettier"
+import storybook from "eslint-plugin-storybook"
+import { includeIgnoreFile } from "@eslint/compat"
+import { fileURLToPath } from "node:url"
 
 const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url))
 
@@ -24,9 +22,32 @@ export default ts.config(
       globals: { ...globals.browser, ...globals.node },
     },
     rules: {
-      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      // Style
       "no-undef": "off",
+      indent: ["error", 2, { "SwitchCase": 1 }],
+      quotes: ["warn", "double"],
+      semi: ["warn", "never"],
+
+      // Good Practices
+      curly: ["warn", "all"],
+      "no-console": "warn",
+      "no-debugger": "error",
+      "no-var": "error",
+      "prefer-const": "warn",
+      "func-style": ["error", "expression"],
+      "prefer-arrow-callback": "warn",
+
+      // Typescript
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "varsIgnorePattern": "^_$",
+          "argsIgnorePattern": "^_$",
+          "ignoreRestSiblings": true
+        }
+      ]
+
     },
   },
   {
